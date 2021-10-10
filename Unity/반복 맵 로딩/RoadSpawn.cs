8bda753d4c027ec29cd9ac2a2ddcab14e5f7199c
20173304 <90365447+20173304@users.noreply.github.com>
2021-10-11 00:23:58 +0900

    Add files via upload

---
+++ Unity/반복 맵 로딩/RoadSpawn.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawn : MonoBehaviour
+{
+    public GameObject LoadRoad;
+
+    Vector3 nextroad;
+
+    public static RoadSpawn instance;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        for(int i = 0; i < 15; i++)
+        {
+            SpawnRoad();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRoad()
+    {
+        GameObject temp = Instantiate(LoadRoad, nextroad, Quaternion.identity);
+        nextroad = temp.transform.GetChild(1).transform.position;
+    }
+}
